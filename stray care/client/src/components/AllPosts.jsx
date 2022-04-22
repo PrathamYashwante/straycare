c8de418bf7bc255b1c64199209779fd2ed3e42f5
yeey <yeey566555@gmail.com>
2022-04-22 22:59:13 +0530

    com

--- stray care/client/src/components/AllPosts.jsx
+++ stray care/client/src/components/AllPosts.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const allposts = () => {
   return (
-    <div>
-
+    <div className="list-group">
+     <table className='table table-hover table-dark'>
+       <thead>
+         <tr className='bg-primary'>
+           
+         </tr>
+       </thead>
+     </table>
 
         
     </div>
